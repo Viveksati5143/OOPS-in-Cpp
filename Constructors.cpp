56049bceaa5b8788ddba1921acff37885ed78afd
Vivek <viveksati2002@gmail.com>
2022-11-20 19:03:56 +0530

    third commit

--- Constructors.cpp
+++ Constructors.cpp
@@ -8,23 +8,56 @@ public:
     string OwnerName;
     int SubscriberCount;
     list<string> PublishedVideoTitles;
+
+    YoutubeChannel(string Name,string ownerName){
+        name = Name;
+        OwnerName = ownerName;
+        SubscriberCount = 0;
+    }
+    void GetInfo(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Owner name:"<<OwnerName<<endl;
+        cout<<"Subscribers Count: "<<SubscriberCount<<endl;
+        cout<<"Videos: "<<endl;
+        for(string videoTitle: PublishedVideoTitles){
+            cout<<videoTitle<<endl;
+        }
+    }
 };
 
 int main()
 {
-    YoutubeChannel ytchannel;
-    ytchannel.name = "jonathan gamimg";
-    ytchannel.OwnerName = "Vivek";
-    ytchannel.SubscriberCount = 500000;
-    ytchannel.PublishedVideoTitles = {"crate opening", "20 kill gameplay", "hacker or wot"};
+    YoutubeChannel ytchannel("jonathan","vivek");
+    ytchannel.PublishedVideoTitles.push_back("crate opening");
+    ytchannel.PublishedVideoTitles.push_back("0 kill gameplay");
+    ytchannel.PublishedVideoTitles.push_back("hacker or wot");
+    // ytchannel.name = "jonathan gamimg";
+    // ytchannel.OwnerName = "Vivek";
+    // ytchannel.SubscriberCount = 500000;
+    // ytchannel.PublishedVideoTitles = {"crate opening", "20 kill gameplay", "hacker or wot"};
 
-    cout<<"Name: "<<ytchannel.name<<endl;
-    cout<<"Owner name:"<<ytchannel.OwnerName<<endl;
-    cout<<"Subscribers Count: "<<ytchannel.SubscriberCount<<endl;
-    cout<<"Videos: "<<endl;
-    for(string videoTitle: ytchannel.PublishedVideoTitles){
-        cout<<videoTitle<<endl;
-    }
-    // system("pause<0");
+    ytchannel.GetInfo();
+    // cout<<"Name: "<<ytchannel.name<<endl;
+    // cout<<"Owner name:"<<ytchannel.OwnerName<<endl;
+    // cout<<"Subscribers Count: "<<ytchannel.SubscriberCount<<endl;
+    // cout<<"Videos: "<<endl;
+    // for(string videoTitle: ytchannel.PublishedVideoTitles){
+    //     cout<<videoTitle<<endl;
+    // }
+
+    // ytchannel.name = "Mafia gamimg";
+    // ytchannel.OwnerName = "Mafia";
+    // ytchannel.SubscriberCount = 900000;
+    // ytchannel.PublishedVideoTitles = {"crate opening", "20 kill gameplay", "tips and tricks"};
+
+    YoutubeChannel ytchannel2("Mafia gaming","mafia");
+    ytchannel2.GetInfo();
+    // cout<<"Name: "<<ytchannel2.name<<endl;
+    // cout<<"Owner name:"<<ytchannel2.OwnerName<<endl;
+    // cout<<"Subscribers Count: "<<ytchannel2.SubscriberCount<<endl;
+    // cout<<"Videos: "<<endl;
+    // for(string videoTitle: ytchannel2.PublishedVideoTitles){
+    //     cout<<videoTitle<<endl;
+    // }
     return 0;   
 }
