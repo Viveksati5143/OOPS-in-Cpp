71a02bc02c39512084a6d1bc81c40d2c0a7624f3
Vivek <viveksati2002@gmail.com>
2022-11-23 05:48:25 +0530

    ninth commit

--- O_O_2.cpp
+++ O_O_2.cpp
@@ -1,9 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class 
+class Complex
+{
+private:
+	int real, imag;
+public:
+	Complex(int r = 0, int i =0){
+         real = r; imag = i; 
+    }
+	friend ostream & operator << (ostream &out, const Complex &c);
+	friend istream & operator >> (istream &in, Complex &c);
+};
 
-int main(){
+ostream & operator << (ostream &out, const Complex &c)
+{
+	out << c.real;
+	out << "+i" << c.imag << endl;
+	return out;
+}
 
-    return 0;
+istream & operator >> (istream &in, Complex &c)
+{
+	cout << "Enter Real Part ";
+	in >> c.real;
+	cout << "Enter Imaginary Part ";
+	in >> c.imag;
+	return in;
+}
+
+int main()
+{
+Complex c1;
+cin >> c1;
+cout << "The complex object is ";
+cout << c1;
+return 0;
 }
